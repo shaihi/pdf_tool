13360180f817e4bd41f76c90f4c5946afdc1a074
shaihi <shaihi@icloud.com>
2025-08-13 15:21:03 +0300

    Remove disallowed export-all barrel from pages/api

--- pages/api/index.js
+++
@@ -1,9 +0,0 @@
-// Auto-generated barrel re-export file
-export * from "../../lib/pdf_core";
-export * from "../../lib/html_render";
-export * from "../../lib/fonts";
-export * from "../../lib/utils";
-export * from "../../lib/api_handler";
-export * from "../../lib/validation";
-export * from "../../lib/file_io";
-export * from "../../lib/other";
